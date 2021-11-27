0eebf3db912bc7d83a19d2f16b4f3cfed37454cb
Afraaz1 <46198568+Afraaz1@users.noreply.github.com>
2021-11-27 02:10:31 +0000

    making code more efficient

--- frontend/src/pages/Results.js
+++ frontend/src/pages/Results.js
@@ -5,25 +5,10 @@ import { Box, Typography} from '@material-ui/core'
 
 const Results = () => {
     const query = useContext(QueryContext);
-    const [dataExists, setDataExists] = useState(true);
-    
-
-    useEffect(() => {
-        console.log(query.animeData)
-        console.log(`test`)
-        if (query.animeData === undefined || query.animeData.length === 0) {
-          try {
-            query.setData(JSON.parse(localStorage.getItem('myData')));
-            setDataExists(true);
-          } catch (error) {
-            console.log(error);
-            setDataExists(false);
-          }
-        }
-      }, [query]);      
+         
       return (
         <Box mt={2}>
-          {(dataExists && <AnimeResults data={query.animeData} />) || (
+          {(query.data.length > 0 && <AnimeResults data={query.animeData} />) || (
             <Typography>No Data Exists</Typography>
           )}
         </Box>
